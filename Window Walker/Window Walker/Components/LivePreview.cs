ed50cb29bb4e4d14df8db5b7746e6e568093e068
Betsegaw Tadele <betsegaw.ta@gmail.com>
2014-02-27 10:47:35 -0800

    Adding forgotten file 'Live Preview'

---
+++ Window Walker/Window Walker/Components/LivePreview.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowWalker.Components
+{
+    /// <summary>
+    /// Class containg methods to control the live preview
+    /// </summary>
+    class LivePreview
+    {
+        /// <summary>
+        /// Makes sure that a window is excluded from the live preview
+        /// </summary>
+        /// <param name="hwnd">handle to the window to exclude</param>
+        public static void SetWindowExlusionFromLivePreview(IntPtr hwnd)
+        {
+            int renderPolicy = (int)InteropAndHelpers.DwmNCRenderingPolicy.Enabled;
+
+            InteropAndHelpers.DwmSetWindowAttribute(
+                hwnd,
+                12,
+                ref renderPolicy,
+                sizeof(int)
+                );
+        }
+
+        /// <summary>
+        /// Activates the live preview
+        /// </summary>
+        /// <param name="targetWindow">the window to show by making all other windows transparent</param>
+        /// <param name="windowToSpare">the window which should not be transparent but is not the target window</param>
+        public static void ActivateLivePreview(IntPtr targetWindow, IntPtr windowToSpare)
+        {
+            InteropAndHelpers.DwmpActivateLivePreview(
+                    true,
+                    targetWindow,
+                    windowToSpare,
+                    InteropAndHelpers.LivePreviewTrigger.Superbar,
+                    IntPtr.Zero);
+        }
+
+        /// <summary>
+        /// Deactivates the live preview
+        /// </summary>
+        public static void DeactivateLivePreview()
+        {
+            InteropAndHelpers.DwmpActivateLivePreview(
+                    false,
+                    IntPtr.Zero,
+                    IntPtr.Zero,
+                    InteropAndHelpers.LivePreviewTrigger.AltTab,
+                    IntPtr.Zero);
+        }
+    }
+}
