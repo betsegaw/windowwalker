f9bcbd2cc7516db8151fc2bef40dbcaf8f07763e
Betsegaw Tadele <betsegaw.ta@gmail.com>
2017-08-27 14:14:38 -0700

    Enable auto-update on hide while the application is running

---
+++ Window Walker/Window Walker/Components/ApplicationUpdates.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Deployment.Application;
+
+namespace WindowWalker.Components
+{
+
+    public class ApplicationUpdates
+    {
+        static DateTime _lastUpdateCheck = DateTime.Now;
+        static int numberOfDaysBetweenCheck = 1;
+
+        public static void InstallUpdateSyncWithInfo()
+        {
+            UpdateCheckInfo info = null;
+
+            var daysSinceLastUpdate = (DateTime.Now - _lastUpdateCheck).Days;
+
+            if (ApplicationDeployment.IsNetworkDeployed && (daysSinceLastUpdate > numberOfDaysBetweenCheck))
+            {
+                ApplicationDeployment ad = ApplicationDeployment.CurrentDeployment;
+
+                try
+                {
+                    info = ad.CheckForDetailedUpdate();
+                }
+                catch {
+                    return;
+                }
+                finally
+                {
+                    _lastUpdateCheck = DateTime.Now;
+                }
+
+                if (info.UpdateAvailable || true)
+                {
+                    try
+                    {
+                        ad.Update();
+                        System.Windows.Application.Current.Shutdown();
+                        System.Windows.Forms.Application.Restart();
+                    }
+                    catch {
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
--- Window Walker/Window Walker/ViewModels/WindowWalkerViewModel.cs
+++ Window Walker/Window Walker/ViewModels/WindowWalkerViewModel.cs
@@ -2,8 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Interop;
 using WindowWalker.Components;
 using WindowWalker.MVVMHelpers;
@@ -217,6 +215,7 @@ namespace WindowWalker.ViewModels
         {
             Components.LivePreview.DeactivateLivePreview();
             this.WindowVisibility = false;
+            ApplicationUpdates.InstallUpdateSyncWithInfo();
         }
 
         private void WindowShow()
